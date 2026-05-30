5edf71dba6e661df599fee44d89b5b650fa74c43
Luis <luistp2utn2026@gmail.com>
2026-05-30 07:05:57 -0300

    TOU-3/TOU-8: Hacer QA de scripts

--- scripts/crea_grafico.py
+++ scripts/crea_grafico.py
@@ -1,17 +1,20 @@
-#Aca esta el grafico que pide la consigna
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Lee el archivo y lo almacena en una variable dataframe
 df = pd.read_csv("../datos/sales_sample_2024.csv")
+#Hace string slicing para extraer solamente el mes de la fecha
 df["mes"] = df["sales_date"].str[5:7]
+#Agrupa filas por mes y suma los valores para tener el total por mes
 ventas_por_mes = df.groupby("mes")["sales_amount"].sum()
 print(ventas_por_mes)
+#Genera un grafica de barras
 ventas_por_mes.plot(kind="bar")
 
+#Establezco el eje x, y, el titulo y guardo el resultado como un archivo .png en la carpeta resultados
 plt.xlabel("Mes")
 plt.ylabel("Ventas")
 plt.title("Ventas por mes")
-#Guardo el archivo como pdf en la carpeta de collab, despues comitear a github
 plt.savefig("../resultados/ventas_por_mes.png")
 
 plt.show()
